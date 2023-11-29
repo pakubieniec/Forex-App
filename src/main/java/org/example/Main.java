3b42be9081f9efb9a475bd21f48c5875be37b63b
pakubieniec <pakubieniec@gmail.com>
2023-11-29 21:58:15 +0100

    Correct code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,29 @@ package org.example;
 
 import services.FetchToTheForexAPI;
 
-import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
         FetchToTheForexAPI fetch = new FetchToTheForexAPI();
+
         System.out.println(fetch.getRespondsWithLatestDate());
         System.out.println(fetch.getRespondsWithHistoricalDate("2023-02-10"));
 
         System.out.println("--------------------------");
         System.out.println("Welcome in Forex App :-)");
         System.out.println("Enter the date (format YYYY-MM-DD) to check the currency rate: ");
-        String date = scanner.nextLine().toLowerCase();
+        String date = scanner.nextLine();
         if (date.isEmpty()) {
             System.out.println(fetch.getRespondsWithLatestDate());
         } else {
             System.out.println(fetch.getRespondsWithHistoricalDate(date));
         }
-        System.out.println("--------------------------");
     }
 }
--- src/main/java/services/FetchToTheForexAPI.java
+++ src/main/java/services/FetchToTheForexAPI.java
@@ -16,7 +16,6 @@ public class FetchToTheForexAPI {
                 .uri(URI.create("http://api.exchangeratesapi.io/v1/latest?access_key=" + API_KEY + "&symbols=PLN"))
                 .GET()
                 .build();
-//        System.out.println(request);
         return getResponds(request);
     }
 
@@ -25,28 +24,13 @@ public class FetchToTheForexAPI {
                 .uri(URI.create("http://api.exchangeratesapi.io/v1/" + date + "?access_key=" + API_KEY + "&symbols=PLN"))
                 .GET()
                 .build();
-//        System.out.println(request);
         return getResponds(request);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FetchToTheForexAPI that)) return false;
-        return Objects.equals(API_KEY, that.API_KEY) && Objects.equals(CLIENT, that.CLIENT);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(API_KEY, CLIENT);
     }
 
     private String getResponds(HttpRequest request) {
         HttpResponse<String> response;
         try {
             response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-//            System.out.println(response);
-
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
